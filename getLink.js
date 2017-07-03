3266df8da81f1d71e1983d306b45527275c17a63
konglingkai <konglingkai@ajmide.com>
2017-07-03 21:25:05 +0800

    update links.

---
+++ getLink.js
@@ -0,0 +1,30 @@
+/**
+ * Created by colin on 2017/7/3.
+ */
+
+const path = require('path');
+const fs   = require('fs');
+const querystring = require('querystring');
+
+fs.readdir(__dirname, (err, files) => {
+    files.forEach(file => {
+        fs.stat(file, (err, stat) => {
+            if(stat.isDirectory()){
+                // read mds
+                fs.readdir(file, (err, mds) => {
+                    mds.forEach(md => {
+                        if(md.indexOf('.md') !== -1){
+                            let title = '* [' + md.replace('.md','') + '] (https://github.com/kopkong/reading-notes/blob/master/';
+
+                            title += querystring.escape(file) + '/' + querystring.escape(md) + ')';
+                            console.log(title);
+                        }
+                    })
+
+                });
+            }
+        });
+    });
+});
+
+
